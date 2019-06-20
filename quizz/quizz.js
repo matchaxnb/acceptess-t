f41f240f1ff2e499ad8c8edadb1a90b9221b4eab
Chloé Desoutter <chloe.desoutter@believedigital.com>
2019-06-20 11:51:23 +0200

    updated quizz

--- quizz/quizz.js
+++ quizz/quizz.js
@@ -5,7 +5,7 @@ for Acceptess-T
 **/
 build_quizz = function(questions, quizz_title, quizz_id) {
     var q_c = 0;
-    var r = $('<div />', {class: 'quizz', text: quizz_title});
+    var r = $('<div />', {class: 'quizz', id: quizz_id, text: quizz_title});
     questions.forEach(function(e) {
         q_c += 1
         var a_c = 0;
@@ -38,6 +38,10 @@ validate_answers = function() {
     $('.reset_quizz').click(function() {
         $('div.question').removeClass('wrong_answer');
         $('div.question').removeClass('right_answer');
+        $('.quizz').removeClass('validated_quizz');
+        $('input:radio').removeAttr('disabled');
+        $('input:radio').prop('checked', false);
+        $('div.results').remove();
     });
     $('input:radio').change(function() {
         if ($(this).hasClass('right_answer')) {
@@ -49,4 +53,18 @@ validate_answers = function() {
             $(this).closest('div.question').removeClass('right_answer');
         }
     })
+}
+
+confirm_quizz = function(quizz_id) {
+    $('input:radio').attr('disabled', 'disabled');
+    $('.quizz').addClass('validated_quizz');
+    var rightans = $('div.question.right_answer').length;
+    var wrongans = $('div.question.wrong_answer').length;
+    var total = $('div.question').length;
+    console.log("right", rightans, "wrong", wrongans);
+    var perc = Math.round(rightans / total * 100);
+    r = $('<div />', { class: 'results' });
+    $('<h2 />', { text: "Votre résultat est de " + perc + "% . Rapprochez-vous des bénévoles !" }).appendTo(r);
+    $('<p />', { text:"(" + rightans + " bonnes réponses, " + wrongans + " mauvaises réponses, " + (total-rightans-wrongans) + " réponses non-apportées)"}).appendTo(r);
+    r.appendTo('#'+quizz_id)
 }
